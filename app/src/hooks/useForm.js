69aff2b7bccdc4c21f0b1efaa6446a67fdb0963d
pmsu2007 <pmsu2007@naver.com>
2024-12-02 14:38:48 +0900

    feat : useForm 커스텀 훅 구현

---
+++ app/src/hooks/useForm.js
@@ -0,0 +1,72 @@
+import {useCallback, useEffect, useState} from "react";
+
+const useForm = ({ initialValues, validate, onSubmit}) => {
+    const [values, setValues] = useState(initialValues);
+    const [errors, setErrors] = useState({});
+    const [touched, setTouched] = useState({});
+    const [disabled, setDisabled] = useState(true);
+
+    const handleChange = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleBlur = (e) => {
+        setTouched({
+            ...touched,
+            [e.target.name]: true,
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // 모든 필드에 방문표시
+        setTouched(
+            Object.keys(values).reduce((touched, field) => {
+                touched[field] = true;
+                return touched
+            }, {})
+        )
+
+        const errors = validate(values)
+        setErrors(errors)
+        if (Object.values(errors).some(v => v)) {
+            return;
+        }
+
+        onSubmit(values)
+    }
+
+
+    // 입력 값에 따라 검증 함수를 실행하는 함수를 정의
+    const runValidator = useCallback(() => {
+        // 폼을 전부 다 입력한 경우
+        if (Object.values(values).every(v => v)) {
+            setDisabled(false);
+        } else {
+            setDisabled(true);
+        }
+
+        return validate(values)
+    }, [values, validate])
+
+    useEffect(() => {
+        const errors = runValidator();
+        setErrors(errors);
+    }, [runValidator])
+
+    return {
+        values,
+        errors,
+        touched,
+        disabled,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+    }
+}
+
+export default useForm;
